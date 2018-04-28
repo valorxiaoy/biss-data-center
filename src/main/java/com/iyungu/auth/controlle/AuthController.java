45d35865361b239b30c81b83c54efe383d9d670b
yueyang <449198512@qq.com>
2018-04-28 19:01:02 +0800

    创建数据中心

--- src/main/java/com/iyungu/auth/controlle/AuthController.java
+++ src/main/java/com/iyungu/auth/controlle/AuthController.java
@@ -3,7 +3,6 @@ package com.iyungu.auth.controlle;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.AbstractUser;
 import io.vertx.ext.auth.AuthProvider;
 import io.vertx.ext.auth.User;
 
@@ -13,10 +12,11 @@ import io.vertx.ext.auth.User;
  * @Date: Created in 14:10 2018/4/26
  * @Modified By:
  */
-public class OAuthController implements AuthProvider {
+public class AuthController implements AuthProvider {
 
     @Override
     public void authenticate(JsonObject jsonObject, Handler<AsyncResult<User>> handler) {
-        AbstractUser abstractUser = new AbstractUser();
+        // AbstractUser abstractUser = new AbstractUser();
     }
+
 }
